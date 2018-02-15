7de9ac5ce5f16f2d606fcffd42454719fff73e1d
yakninja <yakninjayeti@gmail.com>
2018-02-15 12:29:32 +0530

    proper packaging and distribution

--- setup.py
+++ setup.py
@@ -1,13 +1,56 @@
-from distutils.core import setup
+"""A setuptools based setup module.
+
+See:
+https://packaging.python.org/en/latest/distributing.html
+https://github.com/pypa/sampleproject
+"""
+
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+# Arguments marked as "Required" below must be included for upload to PyPI.
+# Fields marked as "Optional" may be commented out.
 
 setup(
-    name='Numerology',
-    version='0.1.0',
-    author='Andrii Kravchuk',
-    author_email='andrii@yakninja.pro',
-    packages=['numerology', 'numerology.vedic', 'numerology.tests'],
-    url='http://pypi.python.org/pypi/Numerology/',
-    license='LICENSE.txt',
-    description='Numerology related stuff.',
-    long_description=open('README.txt').read(),
+    name='Numerology',  # Required
+    version='0.1.0a1',  # Required
+    description='Numerology related stuff',  # Required
+    long_description=long_description,  # Optional
+    url='https://github.com/yakninja/python-numerology',  # Optional
+    author='Andrii Kravchuk',  # Optional
+    author_email='andrii@yakninja.pro',  # Optional
+
+    # Classifiers help users find your project by categorizing it.
+    #
+    # For a list of valid classifiers, see
+    # https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[  # Optional
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Religion',
+        'Topic :: Religion',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+    ],
+    keywords='numerology sanskrit vedic',  # Optional
+
+    # You can just specify package directories manually here if your project is
+    # simple. Or you can use find_packages().
+    #
+    # Alternatively, if you just want to distribute a single Python file, use
+    # the `py_modules` argument instead as follows, which will expect a file
+    # called `my_module.py` to exist:
+    #
+    #   py_modules=["my_module"],
+    #
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),  # Required
 )
